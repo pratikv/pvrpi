baf6d4a7a446ac4b4278d78605219cde2f1fdfc4
pratikv <vakilpratik@gmail.com>
2014-09-18 16:21:36 +0000

    changed the temperature forula

--- code/temp_formula.py
+++ code/temp_formula.py
@@ -2,16 +2,20 @@ import time
 
 def read_file():
   while True:
-	tfile = open( "/sys/bus/w1/devices/10-000800629cc5/w1_slave")
+	tfile = open( "/sys/bus/w1/devices/10-0008006318b6/w1_slave")
 	text = tfile.read()
 	tfile.close()
 	secondline = text.split("\n")[1]
 	temperaturedata = secondline.split(" ")[9]
 	temperature_raw = int(temperaturedata[2:])
-	temperature_raw = (~temperature_raw )+ 1
+	#temperature_raw = temperature_raw + 50
 	temperature = float(temperature_raw)
 	temperature = temperature/1000
+	temperature += 60
 	print temperature
 	time.sleep(1)
 
-read_file()
+try:
+	read_file()
+except:
+	print 'Keyboard interrupted the data'
